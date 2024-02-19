8113f2fe84f129f90c6d4dd267e5d826c2d2a466
jill64 <intents.turrets0h@icloud.com>
2024-02-19 21:13:04 +0900

    refactor Auth class

--- src/auth.ts
+++ src/auth.ts
@@ -22,160 +22,176 @@ const is_auth_code_response = scanner({
 
 type AuthCodeResponse = Guarded<typeof is_auth_code_response>
 
-const gen_redirect_uri = (param: AuthParam) => {
-  const { url, redirect_uri = url.origin } = param
+export class Auth {
+  private env
+  private redirect_uri
+  private scopes
 
-  return encodeURIComponent(redirect_uri)
-}
-
-const save_tokens = async ({ cookies }: AuthParam, data: AuthCodeResponse) => {
-  const { access_token, id_token, refresh_token, expires_in } = data
-
-  cookies.set('access_token', access_token, {
-    ...cookie_config,
-    maxAge: expires_in
-  })
-
-  cookies.set('id_token', id_token, {
-    ...cookie_config,
-    maxAge: expires_in
-  })
-
-  cookies.set('refresh_token', refresh_token, cookie_config)
-}
-
-const exchange_code = async (param: AuthParam) => {
-  const { url, env } = param
-
-  const code = url.searchParams.get('code')
-
-  if (!code) {
-    return null
-  }
-
-  const response = await fetch(`https://${env.COGNITO_DOMAIN}/oauth2/token`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Basic ${Buffer.from(
-        `${env.COGNITO_CLIENT_ID}:${env.COGNITO_CLIENT_SECRET}`
-      ).toString('base64')}`,
-      'Content-Type': 'application/x-www-form-urlencoded'
+  constructor(
+    env: {
+      COGNITO_DOMAIN: string
+      COGNITO_CLIENT_ID: string
+      COGNITO_CLIENT_SECRET: string
     },
-    body: `grant_type=authorization_code&client_id=${
-      env.COGNITO_CLIENT_ID
-    }&code=${code}&redirect_uri=${gen_redirect_uri(param)}`
-  })
-
-  const data = await response.json()
-
-  if (!is_auth_code_response(data)) {
-    console.error('Invalid auth code response', data)
-    return null
+    redirect_uri: string,
+    scopes?: ('openid' | 'profile' | 'email' | 'phone')[]
+  ) {
+    this.env = env
+    this.redirect_uri = redirect_uri
+    this.scopes = scopes ?? ['openid', 'profile', 'email']
   }
 
-  save_tokens(param, data)
-  const cognito_id = await get_user_info(param)
-
-  return cognito_id
-}
-
-const get_user_info = async ({ cookies, env }: AuthParam) => {
-  const access_token = cookies.get('access_token')
-  const id_token = cookies.get('id_token')
-
-  if (!access_token || !id_token) {
-    return null
+  private gen_scoped_param() {
+    return ['aws.cognito.signin.user.admin', ...this.scopes].join('+')
   }
 
-  const response = await fetch(
-    `https://${env.COGNITO_DOMAIN}/oauth2/userInfo`,
-    {
-      headers: {
-        Authorization: `Bearer ${access_token}`
-      }
+  private async save_tokens({ cookies }: AuthParam, data: AuthCodeResponse) {
+    const { access_token, id_token, refresh_token, expires_in } = data
+
+    cookies.set('access_token', access_token, {
+      ...cookie_config,
+      maxAge: expires_in
+    })
+
+    cookies.set('id_token', id_token, {
+      ...cookie_config,
+      maxAge: expires_in
+    })
+
+    cookies.set('refresh_token', refresh_token, cookie_config)
+  }
+
+  private async exchange_code(param: AuthParam) {
+    const { url } = param
+
+    const code = url.searchParams.get('code')
+
+    if (!code) {
+      return null
     }
-  )
 
-  const data = await response.json()
+    const response = await fetch(
+      `https://${this.env.COGNITO_DOMAIN}/oauth2/token`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `${this.env.COGNITO_CLIENT_ID}:${this.env.COGNITO_CLIENT_SECRET}`
+          ).toString('base64')}`,
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `grant_type=authorization_code&client_id=${this.env.COGNITO_CLIENT_ID}&code=${code}&redirect_uri=${this.redirect_uri}`
+      }
+    )
 
-  if (!isUserInfoResponse(data)) {
-    console.error('Invalid user info response', data)
-    return null
+    const data = await response.json()
+
+    if (!is_auth_code_response(data)) {
+      console.error('Invalid auth code response', data)
+      return null
+    }
+
+    this.save_tokens(param, data)
+    const cognito_id = await this.get_user_info(param)
+
+    return cognito_id
   }
 
-  return data
-}
+  private async get_user_info({ cookies }: AuthParam) {
+    const access_token = cookies.get('access_token')
+    const id_token = cookies.get('id_token')
 
-const exchange_token = async (param: AuthParam) => {
-  const { cookies, env } = param
+    if (!access_token || !id_token) {
+      return null
+    }
 
-  const refresh_token = cookies.get('refresh_token')
+    const response = await fetch(
+      `https://${this.env.COGNITO_DOMAIN}/oauth2/userInfo`,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`
+        }
+      }
+    )
 
-  if (!refresh_token) {
-    return null
+    const data = await response.json()
+
+    if (!isUserInfoResponse(data)) {
+      console.error('Invalid user info response', data)
+      return null
+    }
+
+    return data
   }
 
-  const response = await fetch(`https://${env.COGNITO_DOMAIN}/oauth2/token`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Basic ${Buffer.from(
-        `${env.COGNITO_CLIENT_ID}:${env.COGNITO_CLIENT_SECRET}`
-      ).toString('base64')}`,
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: `grant_type=refresh_token&client_id=${env.COGNITO_CLIENT_ID}&refresh_token=${refresh_token}`
-  })
+  private async exchange_token(param: AuthParam) {
+    const { cookies } = param
 
-  const data = await response.json()
+    const refresh_token = cookies.get('refresh_token')
 
-  if (!is_auth_code_response(data)) {
-    console.error('Invalid auth token response', data)
-    return null
-  }
+    if (!refresh_token) {
+      return null
+    }
 
-  save_tokens(param, data)
+    const response = await fetch(
+      `https://${this.env.COGNITO_DOMAIN}/oauth2/token`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `${this.env.COGNITO_CLIENT_ID}:${this.env.COGNITO_CLIENT_SECRET}`
+          ).toString('base64')}`,
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `grant_type=refresh_token&client_id=${this.env.COGNITO_CLIENT_ID}&refresh_token=${refresh_token}`
+      }
+    )
 
-  const user_info = await get_user_info(param)
+    const data = await response.json()
 
-  return user_info
-}
+    if (!is_auth_code_response(data)) {
+      console.error('Invalid auth token response', data)
+      return null
+    }
 
-const gen_scope_param = (param: AuthParam) => {
-  const { scopes = [] } = param
+    this.save_tokens(param, data)
 
-  return ['aws.cognito.signin.user.admin', ...scopes].join('+')
-}
+    const user_info = await this.get_user_info(param)
 
-export const auth = async (param: AuthParam) => {
-  const { env } = param
-
-  const user_info =
-    (await get_user_info(param)) ??
-    (await exchange_token(param)) ??
-    (await exchange_code(param))
-
-  if (user_info) {
     return user_info
   }
 
-  const error_description = param.url.searchParams.get('error_description')
-  const error = param.url.searchParams.get('error')
+  async auth(param: AuthParam) {
+    const user_info =
+      (await this.get_user_info(param)) ??
+      (await this.exchange_token(param)) ??
+      (await this.exchange_code(param))
 
-  if (error && error_description) {
-    throw new Error(
-      JSON.stringify({
-        error,
-        error_description
-      })
+    if (user_info) {
+      return user_info
+    }
+
+    const error_description = param.url.searchParams.get('error_description')
+    const error = param.url.searchParams.get('error')
+
+    if (error && error_description) {
+      throw new Error(
+        JSON.stringify({
+          error,
+          error_description
+        })
+      )
+    }
+
+    const redirect_url = new URL(
+      `https://${this.env.COGNITO_DOMAIN}/oauth2/authorize`
     )
+
+    redirect_url.searchParams.set('client_id', this.env.COGNITO_CLIENT_ID)
+    redirect_url.searchParams.set('response_type', 'code')
+    redirect_url.searchParams.set('scope', this.gen_scoped_param())
+    redirect_url.searchParams.set('redirect_uri', this.redirect_uri)
+
+    return redirect_url
   }
-
-  const redirect_url = new URL(`https://${env.COGNITO_DOMAIN}/oauth2/authorize`)
-
-  redirect_url.searchParams.set('client_id', env.COGNITO_CLIENT_ID)
-  redirect_url.searchParams.set('response_type', 'code')
-  redirect_url.searchParams.set('scope', gen_scope_param(param))
-  redirect_url.searchParams.set('redirect_uri', gen_redirect_uri(param))
-
-  return redirect_url
 }
