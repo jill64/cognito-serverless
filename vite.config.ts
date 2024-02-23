c6a24944d4717e119b7a04e7e8d399261b5c1f61
jill64 <intents.turrets0h@icloud.com>
2024-02-23 09:13:12 +0900

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [sveltekit()],
-  test: {
-    include: ['**/*.test.ts']
-  },
   ssr: {
     noExternal: ['@jill64/sentry-sveltekit-cloudflare']
-  },
-  server: {
-    fs: {
-      allow: ['.']
-    }
   }
 })
